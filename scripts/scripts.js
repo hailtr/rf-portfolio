953b5dc0e4e33b1d3213e10a312b3c2c0d456444
hailtr <lustigrfortiz.com>
2025-05-19 11:29:53 -0400

    * Glass y shades añadidos * Inicio de reforma de tarjetas.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,14 @@
-// No funcionó así que vamos a intentar otras cosas.
+// Date: 2025-02-06 (6th February 2025)
+// Version: 1.0.1
+// Description: JavaScript file for the portfolio website
 
+// Function to toggle the menu
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navBanner = document.querySelector("#nav-banner");
   
+    menuToggle.addEventListener("click", function () {
+      navBanner.classList.toggle("active");
+    });
+  });
   
